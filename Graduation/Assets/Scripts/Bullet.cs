8aaa773bc73448c3efdaea3c2a97ac16f564015c
Petar Markov <petar.g.markov@protonmail.com>
2022-06-13 02:26:38 +0300

    Removing the fucking rars because Martin is an idiot, combining the two games and making the player shoot

---
+++ Graduation/Assets/Scripts/Bullet.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float VelX = 3f;
+    public float VelY = 0f;
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2 (VelX, VelY);
+        Destroy (gameObject, 4f);
+    }
+}
--- Graduation/Assets/Scripts/movement.cs
+++ Graduation/Assets/Scripts/movement.cs
@@ -3,14 +3,48 @@ using UnityEngine;
 public class movement : MonoBehaviour
 {
 	public float speed = 10f;
+	public GameObject BulletDown , BulletUp , BulletToRight , BulletToLeft;
+	Vector2 BulletPos;
+	public float fireRate = 0.5f;
+	public float nextFire = 0f;
+	
     // Update is called once per frame
     void Update()
     {
-		float HorizontalMovement = Input.GetAxis("Horizontal")*speed;
-		float VerticalMovement = Input.GetAxis("Vertical")*speed;
+		float VerticalMovement = 0f;
+		float HorizontalMovement = 0f;
 		HorizontalMovement *= Time.deltaTime;
 		VerticalMovement *= Time.deltaTime;
-		transform.Translate(HorizontalMovement, 0, 0); 
-		transform.Translate(0, VerticalMovement, 0);
+		
+			VerticalMovement = Input.GetAxis("Vertical")*speed;
+			VerticalMovement *= Time.deltaTime;
+			transform.Translate(0,VerticalMovement,0);
+			HorizontalMovement = Input.GetAxis("Horizontal")*speed;
+			HorizontalMovement *= Time.deltaTime;
+			transform.Translate(HorizontalMovement,0,0);
+		
+		if (Input.GetMouseButtonDown(0) && Time.time > nextFire){
+			nextFire = Time.time + fireRate;
+			fire();
+		}
+	}
+	void fire(){
+		var MousePosition = Input.mousePosition;
+		var PlayerPosition = Camera.main.WorldToScreenPoint(transform.position);
+		BulletPos = transform.position;
+		if (MousePosition.x > PlayerPosition.x && PlayerPosition.x/(MousePosition.x+1) < PlayerPosition.y/MousePosition.y){
+			BulletPos += new Vector2 (0.1f, 0f);
+			Instantiate(BulletToRight, BulletPos, Quaternion.identity);
+		}else if(MousePosition.x < PlayerPosition.x && PlayerPosition.x/MousePosition.x >PlayerPosition.y/(MousePosition.y+1)){
+			BulletPos += new Vector2 (-0.3f, 0f);
+			Instantiate(BulletToLeft, BulletPos, Quaternion.identity);
+		}
+		if (MousePosition.y>PlayerPosition.y&& PlayerPosition.x/MousePosition.x > PlayerPosition.y/(MousePosition.y+1)){
+			BulletPos += new Vector2 (0.5f, 0f);
+			Instantiate(BulletUp, BulletPos, Quaternion.identity);
+		}else if (MousePosition.y<PlayerPosition.y && PlayerPosition.x/(MousePosition.x+1) < PlayerPosition.y/MousePosition.y){
+			BulletPos += new Vector2 (-0.5f, 0f);
+			Instantiate(BulletDown, BulletPos, Quaternion.identity);
+		}
 	}
 }
